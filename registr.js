615056ebde998cce92c3591a2851001ba0b0dcae
Norainu543 <96815555+Norainu543@users.noreply.github.com>
2021-12-29 11:11:25 +0300

    Add files via upload

---
+++ registr.js
@@ -0,0 +1,17 @@
+function vivod(){
+	var familia = document.getElementById('familia').value;
+	var ima = document.getElementById('ima1').value;
+	var otchestvo = document.getElementById('otchestvo').value;
+	var pochta = document.getElementById('pochta').value;
+	var strana = document.getElementById('strana').value;
+	var gorod = document.getElementById('gorod').value;
+	var parol = document.getElementById('parol').value;
+	var eparol = document.getElementById('eparol').value;
+	document.write("Ваша фамилия: " + familia +"<br>");    
+	document.write("Ваше имя: " + ima +"<br>");
+	document.write("Ваше отчество: " + otchestvo +"<br>");
+	document.write("Ваша почта: " + pochta +"<br>");
+	document.write("Ваша страна: " + strana +"<br>");
+	document.write("Ваш город: " + gorod +"<br>");
+	document.write("Ваш пароль: " + parol +"<br>");
+}
